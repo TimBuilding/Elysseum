98b058ff27c8f999e16c15b2ede5e4faa85dbcec
xdjeremy <jeremy.fran@icloud.com>
2024-05-04 18:23:45 +0800

    feat(services): add aceternity sticky-scroll component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'sans': ['Inter']
-			}
 
-		},
-	},
-	plugins: [],
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Inter']
+            }
+
+        },
+    },
+    plugins: [addVariablesForColors],
+}
+
+function addVariablesForColors({addBase, theme}) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+
+    addBase({
+        ":root": newVars,
+    });
 }

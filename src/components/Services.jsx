c8623358272edf4a379273d0a5a73643938f0822
xdjeremy <44615921+xdjeremy@users.noreply.github.com>
2024-05-05 02:38:10 +0000

    feat: add proper text for services (#4)

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,65 +1,94 @@
 import React from 'react';
-import {StickyScroll} from "@/components/ui/sticky-scroll-reveal.jsx";
+import { StickyScroll } from '@/components/ui/sticky-scroll-reveal.jsx';
 
 const content = [
-    {
-        title: "IT Strategy & Planning",
-        description:
-            "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
-        content: (
-            <div
-                className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-                Collaborative Editing
-            </div>
-        ),
-    },
-    {
-        title: "Real time changes",
-        description:
-            "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
-        content: (
-            <div className="h-full w-full  flex items-center justify-center text-white">
-                <img
-                    src="/linear.webp"
-                    width={300}
-                    height={300}
-                    className="h-full w-full object-cover"
-                    alt="linear board demo"
-                />
-            </div>
-        ),
-    },
-    {
-        title: "Version control",
-        description:
-            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-        content: (
-            <div
-                className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-                Version control
-            </div>
-        ),
-    },
-    {
-        title: "Running out of content",
-        description:
-            "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
-        content: (
-            <div
-                className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-                Running out of content
-            </div>
-        ),
-    },
+  {
+    title: 'IT Strategy & Planning',
+    description:
+      'We work closely with businesses to develop IT strategies that align with their objectives. For instance, we helped our client assess their current IT infrastructure, identify areas for improvement, and create a roadmap to enhance efficiency and productivity.',
+    content: (
+      <div className='h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white'>
+        Collaborative Editing
+      </div>
+    ),
+  },
+  {
+    title: 'Digital Transformation',
+    description:
+      'We guide organizations through digital transformation by helping them adopt new technologies and reshape their business processes. Our expertise includes cloud computing, automation, data analytics, and emerging technologies.',
+    content: (
+      <div className='h-full w-full  flex items-center justify-center text-white'>
+        <img
+          src='/linear.webp'
+          width={300}
+          height={300}
+          className='h-full w-full object-cover'
+          alt='linear board demo'
+        />
+      </div>
+    ),
+  },
+  {
+    title: 'IT Infrastructure Design & Implementation',
+    description:
+      "We design and implement robust and scalable IT infrastructures tailored to our clients' needs. This includes network architecture, hardware and software selection, cybersecurity measures, and disaster recovery planning.",
+    content: (
+      <div className='h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white'>
+        Version control
+      </div>
+    ),
+  },
+  {
+    title: 'Cybersecurity',
+    description:
+      'We provide comprehensive cybersecurity services to protect businesses from evolving threats. This includes risk assessments, vulnerability assessments, security audits, implementation of security controls, employee training, incident response, and ongoing monitoring.',
+    content: (
+      <div className='h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white'>
+        Running out of content
+      </div>
+    ),
+  },
+  {
+    title: 'IT Project Management',
+    description:
+      'We manage IT projects from initiation to completion, ensuring successful implementation within scope, budget, and timeline. Our project management approach includes effective communication, stakeholder engagement, risk management, and quality assurance.',
+    content: (
+      <div className='h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white'>
+        Running out of content
+      </div>
+    ),
+  },
+  {
+    title: 'IT Support & Maintenance',
+    description:
+      "We offer ongoing IT support services to keep our clients' systems running smoothly. This includes help desk support, troubleshooting, system updates, performance monitoring, and proactive maintenance.",
+    content: (
+      <div className='h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white'>
+        Running out of content
+      </div>
+    ),
+  },
+  {
+    title: 'IT Training & Education',
+    description:
+      "We provide training programs and workshops to empower our clients' employees with the necessary IT skills and knowledge. This includes technology adoption training, cybersecurity awareness, and best practices for optimizing productivity.",
+    content: (
+      <div className='h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white'>
+        Running out of content
+      </div>
+    ),
+  },
 ];
 
 const Services = () => {
-    return (
-        <div className={'p-10 w-full flex flex-col items-center justify-center mt-32'}>
-            {/* todo: add fade */}
-            <StickyScroll content={content}/>
-        </div>
-    );
+  return (
+    <div
+      className={'p-10 w-full flex flex-col items-center justify-center mt-32'}
+    >
+      {/* todo: add fade */}
+      <StickyScroll content={content} />
+    </div>
+  );
 };
 
 export default Services;

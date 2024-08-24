7c2326a684e1588cd37b94e4542cc2a9997bf428
tamrivera05 <tamangelarivera@gmail.com>
2024-08-24 10:09:31 +0000

    Fix build errors (#39)

---
+++ src/components/ContactFormComponent.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Input from '@/components/ui/input';
+import Textarea from '@/components/ui/textarea';
+
+interface ContactFormProps {
+    title: string;
+    placeholder: string;
+    type?: 'input' | 'textarea';
+}
+
+const ContactFormComponent: React.FC<ContactFormProps> = ({title, placeholder, type}) => {
+    return (
+        <div className="gap-4 flex flex-col lg:flex-row lg:my-1">
+            <p className="w-1/4 text-base font-medium text-[#E6E6E6] text-left lg:text-xl">{title}</p>
+            {type === 'textarea' ? (
+                <Textarea rows="4" placeholder={placeholder}/>
+            ) : (
+                <Input type="text" placeholder={placeholder}/>
+            )}
+        </div>
+    );
+};
+
+export default ContactFormComponent;
+
+
--- src/components/ContactFormProps.tsx
+++ src/components/ContactFormProps.tsx
@@ -1,15 +1,16 @@
+import React from "react";
 import type {FC} from "react";
-import ContactForm from "../components/ContactForm.astro";
+import ContactFormComponent from "./ContactFormComponent.tsx";
 
 export interface ContactFormProps {
     title: string
     placeholder: string
     type?: 'input' | 'textarea'
-};
+}
 
 const ContactForm: FC<ContactFormProps> = ({ title, placeholder, type }) => {
     return (
-        <ContactForm title={title} placeholder={placeholder} type={type}></ContactForm>
+        <ContactFormComponent title={title} placeholder={placeholder} type={type}></ContactFormComponent>
     )
 }
 
